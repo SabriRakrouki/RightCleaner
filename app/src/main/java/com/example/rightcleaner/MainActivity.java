58b43905609fb78395558f3533f7b2b64832545b
mohamed-amine-aouadi <48270211+mohamed-amine-aouadi@users.noreply.github.com>
2022-10-29 23:23:01 +0100

    Signup page

--- app/src/main/java/com/example/rightcleaner/MainActivity.java
+++ app/src/main/java/com/example/rightcleaner/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.rightcleaner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
+       /* Button test = findViewById(R.id.button5);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("test hello");
+            }
+        });*/
     }
+
+
+
 }
