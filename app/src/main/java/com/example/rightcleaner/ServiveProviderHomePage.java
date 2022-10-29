e39b06daaf232652d63dc37bc930728b6f4bcf27
unknown <sabri.rakrouki@esprit.tn>
2022-10-30 00:22:54 +0200

    add

--- app/src/main/java/com/example/rightcleaner/ServiveProviderHomePage.java
+++ app/src/main/java/com/example/rightcleaner/ServiveProviderHomePage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ServiveProviderHomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        setContentView(R.layout.activity_servive_provider_home_page);
     }
 }
