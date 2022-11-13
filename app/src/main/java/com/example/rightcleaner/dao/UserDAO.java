bd73f999d5611c308ab2b2f7071180294085ab7b
unknown <sabri.rakrouki@esprit.tn>
2022-11-13 01:56:22 +0100

    sign up with button problem

---
+++ app/src/main/java/com/example/rightcleaner/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package com.example.rightcleaner.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.rightcleaner.entity.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+    @Query("SELECT * FROM users")
+    List<User> getAll();
+    @Insert
+    void register(User user);
+    @Update
+    void updateUser(User user);
+    @Delete
+    void deleteUser(User user);
+
+}
