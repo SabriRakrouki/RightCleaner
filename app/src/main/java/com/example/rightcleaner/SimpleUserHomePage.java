6ca27c1b2209e979e1b38443409441dd69741860
mohamed-amine-aouadi <48270211+mohamed-amine-aouadi@users.noreply.github.com>
2022-11-17 22:05:18 +0100

    choose Service page

--- app/src/main/java/com/example/rightcleaner/SimpleUserHomePage.java
+++ app/src/main/java/com/example/rightcleaner/SimpleUserHomePage.java
@@ -9,6 +9,6 @@ public class SimpleUserHomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simple_user_home_page);
+        setContentView(R.layout.choose_service_page);
     }
 }
