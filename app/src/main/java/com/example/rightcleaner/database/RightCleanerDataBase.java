b2f360b8e4dd2850596c41c76f8ee5fa38a03d6f
mohamed-amine-aouadi <48270211+mohamed-amine-aouadi@users.noreply.github.com>
2022-11-18 12:08:56 +0100

    Meet page

--- app/src/main/java/com/example/rightcleaner/database/RightCleanerDataBase.java
+++ app/src/main/java/com/example/rightcleaner/database/RightCleanerDataBase.java
@@ -9,18 +9,21 @@ import androidx.room.RoomDatabase;
 import androidx.room.migration.AutoMigrationSpec;
 import androidx.room.migration.Migration;
 
+import com.example.rightcleaner.dao.MeetDAO;
 import com.example.rightcleaner.dao.ReviewDAO;
 import com.example.rightcleaner.dao.UserDAO;
 import com.example.rightcleaner.dao.UserServiceProviderDAO;
+import com.example.rightcleaner.entity.Meet;
 import com.example.rightcleaner.entity.Review;
 import com.example.rightcleaner.entity.User;
 import com.example.rightcleaner.entity.UserServiceProvider;
 
 
-@Database(entities = {User.class, UserServiceProvider.class, Review.class},version = 2, exportSchema = false)
+@Database(entities = {User.class, UserServiceProvider.class, Review.class, Meet.class},version = 2, exportSchema = false)
 
 public abstract class RightCleanerDataBase extends RoomDatabase {
     public abstract UserDAO userDAO();
+    public abstract MeetDAO meetDAO();
     public abstract ReviewDAO reviewDAO();
     public abstract UserServiceProviderDAO userServiceProviderDAO();
     private static final String dbName="RightCleaner";
---
+++ app/src/main/java/com/example/rightcleaner/entity/Meet.java
@@ -0,0 +1,59 @@
+package com.example.rightcleaner.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "meet")
+public class Meet {
+    @PrimaryKey(autoGenerate = true)
+    Integer id;
+    @ColumnInfo(name = "description")
+    String description;
+    @ColumnInfo(name = "emailSimpleUser")
+    String emailSimpleUser;
+    @ColumnInfo(name = "emailServiceUser")
+    String emailServiceUser;
+    @ColumnInfo(name = "date")
+    String date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmailSimpleUser() {
+        return emailSimpleUser;
+    }
+
+    public void setEmailSimpleUser(String emailSimpleUser) {
+        this.emailSimpleUser = emailSimpleUser;
+    }
+
+    public String getEmailServiceUser() {
+        return emailServiceUser;
+    }
+
+    public void setEmailServiceUser(String emailServiceUser) {
+        this.emailServiceUser = emailServiceUser;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
